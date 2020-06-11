16f55938a99c1758f064535cafa2562c8afc899b
TsFoteini <foteinitsavo@gmail.com>
2020-06-12 01:25:55 +0300

    added map

--- Content/js/index.js
+++ Content/js/index.js
@@ -37,6 +37,22 @@ $("#enddate").change(function () {
 
 Get_Data(WebSiteUrl + "/Home/GetApartmentsTypes");
 
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+    })
+});
+
+map.on('click', function (evt) {
+    var coordinates = evt.coordinate;
+});
 
 
 
