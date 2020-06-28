c6e2dc489859582266e7767cf208754c132cc672
TsFoteini <foteinitsavo@gmail.com>
2020-06-28 23:29:21 +0300

    updating

--- Content/js/accountsettings.js
+++ Content/js/accountsettings.js
@@ -36,3 +36,29 @@ function Get_Usernames_Request(url, data) {
 
     });
 }
+
+function UserSubmit() {
+    var role = UserApply();
+    var username = document.getElementById("userusrname1").value;
+    var password = document.getElementById("userpass1").value;
+    var email = document.getElementById("usermail").value;
+    var name = document.getElementById("username").value;
+    var surname = document.getElementById("usersurname").value;
+    var mobile = document.getElementById("usermobile").value;
+    var photo = document.getElementById("userphoto").value;
+    var data = { usrname: username, pass: password, email: email, name: name, surname: surname, phone_number: mobile, images: photo, role_id: role }
+    Put_Data_Register(WebSiteUrl + '/AccountSettings/UpdateAccount', data);
+}
+
+async function Put_Data_Register(url, data) {
+    await $.ajax(url, {
+        method: "PUT",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data)
+    }).done(function (result) {
+
+    }).fail(function (xhr) {
+
+    });
+}
--- Controllers/AccountSettingsController.cs
+++ Controllers/AccountSettingsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 using WebHome.Domain.IServices;
 using WebHome.Domain.Models;
 using WebHome.Domain.Services;
@@ -25,5 +26,46 @@ namespace WebHome.Controllers
             current_user = usr_model.user;
             return current_user;
         }
+
+        [HttpPut]
+        public IActionResult UpdateAccount([FromBody] User values)
+        {
+            try
+            {
+                var db = new DB();
+                var Query = @"Update register set  role_id= @m_role_id , usrname = @m_usrname, pass=@m_pass , name=@m_name, surname=@m_surname,phone_number=@m_phone_number, 
+                                images=@m_images,email=@m_email,accepted=@m_accepted where user_id = @m_user_id";
+                var cmd = new NpgsqlCommand();
+                cmd.CommandText = Query;
+                cmd.Parameters.AddWithValue("@m_images", values.images);
+                cmd.Parameters.AddWithValue("@m_email", values.email);
+                cmd.Parameters.AddWithValue("@m_phone_number", values.phone_number);
+                cmd.Parameters.AddWithValue("@m_usrname", values.usrname);
+                cmd.Parameters.AddWithValue("@m_pass", values.pass);
+                cmd.Parameters.AddWithValue("@m_name", values.name);
+                cmd.Parameters.AddWithValue("@m_surname", values.surname);
+                cmd.Parameters.AddWithValue("m_created_on", DateTime.Now);
+                cmd.Parameters.AddWithValue("@m_user_id", values.user_id);
+                cmd.Parameters.AddWithValue("@m_role_id", values.role_id);
+                if (values.role_id == 1 || values.role_id == 3)
+                {
+                    cmd.Parameters.AddWithValue("@m_accepted", 0);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@m_accepted", 1);
+                }
+                cmd.Connection = db.npgsqlConnection;
+                cmd.ExecuteNonQuery();
+                db.close();
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Ok();
+        }
+       
     }
 }
