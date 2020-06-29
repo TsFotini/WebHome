144822c32e31e98cdba1bc8ef8683e599db664bd
TsFoteini <foteinitsavo@gmail.com>
2020-06-29 23:18:46 +0300

    added images

--- Content/js/index.js
+++ Content/js/index.js
@@ -58,4 +58,17 @@ $("#enddate").change(function () {
 Get_Data(WebSiteUrl + "/Home/GetApartmentsTypes");
 Get_Places(WebSiteUrl + "/Home/GetPlaces");
 
+var myIndex = 0;
+carousel();
 
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
